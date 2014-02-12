45db596b2bf2f29e2c5066361ed86484efc72d55
Rob Goeddel <rgoeddel@umich.edu>
2014-02-12 04:01:29 -0500

    slight update, but not working properly for some reason

--- gui.c
+++ gui.c
@@ -190,11 +190,9 @@ int renderCameraPOVLayer(state_t *state, layer_data_t *layerData) {
         //might wanna make diff d.s.
         //Also, gonna need to copy image
         //Green
-	/*
         num_balls = blob_detection(im, balls,
-                        0xff394d2c, 0xffe127ff,
+                        0xff30523d, 0xffff0000,
                         100.0);
-			*/
 
         vx_object_t * vo = vxo_image_from_u32(im, VXO_IMAGE_FLIPY,
 		VX_TEX_MIN_FILTER | VX_TEX_MAG_FILTER);
