85c5c381b61c2a1da6e479f7d1ff53f158c05aec
Bruce <bruce@gmail.com>
2014-02-18 07:58:11 -0500

    fixed josh's crappy driver_monitor

--- driver_monitor.sh
+++ driver_monitor.sh
@@ -1,28 +1,32 @@
 #! /bin/sh
 
-#trap exiting SIGINT
+trap exiting SIGINT
 
-#exiting() { echo "Received ctrl-c\n"; }
+EXITED=0
+exiting() { EXITED=1; }
 
 echo "Starting maebot driver"
 
 #while [ `pgrep -f "./maebot_app"` ]
 #do
-while [ ! `pgrep -f "./maebot_driver"` ]
+while [[ ! `pgrep -f "./maebot_driver"` ]]
 do
 	./maebot_driver
 done
 #done
 echo "Maebot driver started successfully"
 
-while [ `pgrep -f "./maebot_app"` ]
+CYCLES=0
+
+while [[ `pgrep -f "./maebot_app"` && $EXITED -eq 0 ]]
 do
 #Just hang out til the bot app closes
+CYCLES=$CYCLES+1
 done
 
 echo "Killing maebot driver"
 
-if [ `pgrep -f "./maebot_driver"`]
+if [[ `pgrep -f "./maebot_driver"` ]]
 then
 	kill `pgrep -f "./maebot_driver"`
 fi
