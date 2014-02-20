951346af1e6dc2072925b832229e1148e1b941d5
Josh <josh@josh-VirtualBox.(none)>
2014-02-20 13:11:41 -0500

    new driver monitor

--- driver_monitor.sh
+++ driver_monitor.sh
@@ -1,17 +1,28 @@
 #! /bin/sh
 
-trap exiting SIGINT
+#trap exiting SIGINT
 
-exiting() { echo "Received ctrl-c\n"; }
+#exiting() { echo "Received ctrl-c\n"; }
 
-echo "Starting rexarm driver monitor script"
+echo "Starting maebot driver"
+
+#while [ `pgrep -f "./maebot_app"` ]
+#do
+while [ ! `pgrep -f "./maebot_driver"` ]
+do
+	./maebot_driver
+done
+#done
+echo "Maebot driver started successfully"
 
 while [ `pgrep -f "./maebot_app"` ]
 do
-	if [ ! `pgrep -f "./maebot_driver"` ]
-	then
-		./maebot_driver
-	fi
+#Just hang out til the bot app closes
 done
 
+echo "Killing maebot driver"
 
+if [ `pgrep -f "./maebot_driver"`]
+then
+	kill `pgrep -f "./maebot_driver"`
+fi
